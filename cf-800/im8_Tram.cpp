422097b5b38a854a601c8cd64a04ef3972c3069e
ARAFAT <munshiarafathussain@gmail.com>
2023-12-26 12:43:51 +0600

    cf-800 3 problem

---
+++ cf-800/im8_Tram.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define ONLINE_JUDGE
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a,i<b;i++)
+#define ff first
+#define ss second
+
+
+
+int main() {
+    #ifdef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, curr=0, ans=0;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int a, b;
+        cin>>a>>b;
+        curr = (curr-a + b);
+
+        ans = max(ans, curr);
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
