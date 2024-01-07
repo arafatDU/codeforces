b1c7f00e0f4b04ad79a5272f0ffb0df38e87f67d
ARAFAT <munshiarafathussain@gmail.com>
2024-01-07 12:31:21 +0600

    cf-800 solved 3

---
+++ cf-800/gr5_RemoveSmallest.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ONLINE_JUDGE
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+
+
+int main()
+{
+    #ifdef ONLINE_JUDGE
+          freopen("input.txt", "r", stdin);
+          freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        bool ans = true;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++) cin>>arr[i];
+        sort(arr, arr+n);
+        for(int i=1; i<n; i++){
+            if(arr[i] - arr[i-1] > 1) ans = false;
+        }
+        cout<<(ans? "YES": "NO")<<endl;
+    }
+
+    return 0;
+}
